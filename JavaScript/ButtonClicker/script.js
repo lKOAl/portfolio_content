6dcb89d4b7c39970d4d6463037c5949361d9ebec
lKOAl <justinnguyen25@ymail.com>
2022-02-02 14:54:31 -0600

    commit

--- JavaScript/ButtonClicker/script.js
+++ JavaScript/ButtonClicker/script.js
@@ -1,2 +1,17 @@
-function change-text(){
-    document.getElementById('future-clicked').innerHTML = 'Logout';
+console.log("page loaded...");
+
+function message() {
+    alert("Ninja was liked!");
+}
+
+function login(element) {
+    if (element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(el) {
+    el.remove();
+}
